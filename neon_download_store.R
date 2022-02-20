b690d5bfab867dcc85ecdf35360675ce437e7822
rqthomas <rqthomas@vt.edu>
2022-02-20 12:25:21 +0000

    last updates from server

--- neon_download_store.R
+++ neon_download_store.R
@@ -7,30 +7,30 @@ Sys.setenv("duckdb_restart"="TRUE")
 
 
 ## Terrestrial
-#DP4.00200.001 & DP1.00094.001
-site_table <- read_csv("https://raw.githubusercontent.com/eco4cast/neon4cast-terrestrial/master/Terrestrial_NEON_Field_Site_Metadata_20210928.csv")
-sites <- site_table$field_site_id
-
-print("Downloading: DP4.00200.001")
-neonstore::neon_download(product = "DP4.00200.001", site = sites, type = "basic")
-neon_store(product = "DP4.00200.001") 
-#print("Downloading: DP1.00094.001")
-#x <- neonstore::neon_download(product = "DP1.00094.001", site = sites, type = "basic")
-#neon_store(table = "SWS_30_minute", n = 50) 
+# #DP4.00200.001 & DP1.00094.001
+# site_table <- read_csv("https://raw.githubusercontent.com/eco4cast/neon4cast-terrestrial/master/Terrestrial_NEON_Field_Site_Metadata_20210928.csv")
+# sites <- site_table$field_site_id
+# 
+# print("Downloading: DP4.00200.001")
+# neonstore::neon_download(product = "DP4.00200.001", site = sites, type = "basic")
+# neon_store(product = "DP4.00200.001") 
+# #print("Downloading: DP1.00094.001")
+# #x <- neonstore::neon_download(product = "DP1.00094.001", site = sites, type = "basic")
+# #neon_store(table = "SWS_30_minute", n = 50) 
 
 ## Aquatics
-site_table <- read_csv("https://raw.githubusercontent.com/eco4cast/neon4cast-aquatics/master/Aquatic_NEON_Field_Site_Metadata_20210928.csv")
-sites <- site_table$field_site_id
+#site_table <- read_csv("https://raw.githubusercontent.com/eco4cast/neon4cast-aquatics/master/Aquatic_NEON_Field_Site_Metadata_20210928.csv")
+#sites <- site_table$field_site_id
 
-message("Downloading: DP1.20288.001")
-neonstore::neon_download("DP1.20288.001",site = sites, type = "basic")
-neonstore::neon_store(table = "waq_instantaneous", n = 50)
-message("Downloading: DP1.20264.001")
-neonstore::neon_download("DP1.20264.001", site =  sites, type = "basic")
-neonstore::neon_store(table = "TSD_30_min")
-message("Downloading: DP1.20053.001")
-neonstore::neon_download("DP1.20053.001", site =  sites, type = "basic")
-neonstore::neon_store(table = "TSW_30min")
+#message("Downloading: DP1.20288.001")
+#neonstore::neon_download("DP1.20288.001",site = sites, type = "basic")
+#neonstore::neon_store(table = "waq_instantaneous", n = 50)
+#message("Downloading: DP1.20264.001")
+#neonstore::neon_download("DP1.20264.001", site =  sites, type = "basic")
+#neonstore::neon_store(table = "TSD_30_min")
+#message("Downloading: DP1.20053.001")
+#neonstore::neon_download("DP1.20053.001", site =  sites, type = "basic")
+#neonstore::neon_store(table = "TSW_30min")
 
 
 ## Beetle
